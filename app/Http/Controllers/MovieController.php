7df8e2e33ae16743c295e030b9b8e9a7149a0f72
Skyfe24 <fabio.erase@gmail.com>
2023-08-22 17:37:10 +0200

    created MovieController

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $movies = config('Movie');
+        return view('Movie.index', compact('movies'));
+    }
+}
