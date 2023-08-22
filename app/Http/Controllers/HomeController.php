104652b968b036ec2222d0ce2c9cc01b3907d41a
Skyfe24 <fabio.erase@gmail.com>
2023-08-22 17:05:49 +0200

    typo fixed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
     public function index()
     {
         $movies = config('Movie');
-        return view('Movie,index', compact('books'));
+        return view('Movie,index', compact('movies'));
 
 
     }
