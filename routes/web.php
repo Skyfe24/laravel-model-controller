817dbb775fa183083c05d51f6dc5b0a68224b15c
Skyfe24 <fabio.erase@gmail.com>
2023-08-22 17:20:33 +0200

    created movie.blade.php and created the relative route

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 //  });
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/', [HomeController::class, 'index'])->name('movie');
