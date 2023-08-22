566a7df0fa9c07ed6e5df83478c653781cf24273
Skyfe24 <fabio.erase@gmail.com>
2023-08-22 17:24:50 +0200

    put text on movie.blade.php

---
+++ resources/views/movie.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset=“utf-8”>
+    <meta name=“viewport” content=“width=device-width, initial-scale=1”>
+    <title>Laravel</title>
+    <!-- Fonts -->
+    <link href=“https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&displayswap” rel=“stylesheet”>
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <main class=“bg-light”>
+        <div class=“container”>
+            <div class=“row”>
+                <div class=“col-12”>
+                    <div class=“min-vh-100 py-5 d-flex flex-column justify-content-center”>
+                        <h1>Benvenuti su Movie.Blade!</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
